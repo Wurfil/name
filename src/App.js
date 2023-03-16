098783e0da507271228128e8817f8749d003305d
wurfil <52235346+Wurfil@users.noreply.github.com>
2023-03-16 21:49:11 +0300

    get data from internet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import {useMemo, useState} from "react";
+import {useEffect, useState} from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./components/hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./components/hooks/useFetching";
 
 function App() {
 
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'GGDFsad ___1___', body: 'dsaasdadasd text'},
-        {id: 2, title: 'Hedasd ___2___', body: 'Someeee dasdasd'},
-        {id: 3, title: 'HeadJdaasdasd ___3___', body: 'dasd text'}
-    ])
+    const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
-    const  sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
+    const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll()
+        setPosts(posts)
+    })
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
     }
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
@@ -35,7 +41,13 @@ function App() {
             </MyModal>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} serFilter={setFilter}/>
-            <PostList remove={removePost} posts={sortedAndSearchPosts} title="0001"/>
+            {postError &&
+                <h1>Произошла ошибка ${postError}</h1>
+            }
+            {isPostsLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
+                : <PostList remove={removePost} posts={sortedAndSearchPosts} title="0001"/>
+            }
         </div>
     );
 }
