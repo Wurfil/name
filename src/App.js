d4bbeeffa9c9b0c2d9815045b62bb9afc3e87a20
wurfil <52235346+Wurfil@users.noreply.github.com>
2023-03-16 21:09:53 +0300

    decomposition

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./components/hooks/usePosts";
 
 function App() {
 
@@ -14,16 +15,7 @@ function App() {
     ])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts
-    }, [filter.sort, posts])
-
-    const sortedAndSearchPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-    }, [filter.query, sortedPosts])
+    const  sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
